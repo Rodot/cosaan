26d7def05064aef5e8515722e36d18a374f58b32
Aurelien Rodot <4189146+Rodot@users.noreply.github.com>
2025-04-30 16:09:22 +0200

    Fix lint

--- app/lib/presentation/components/app_loading_bar.dart
+++ app/lib/presentation/components/app_loading_bar.dart
@@ -1,5 +1,4 @@
 import 'package:app/presentation/state/logs_provider.dart';
-import 'package:app/presentation/utils/show_error_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:app/presentation/state/profile_provider.dart';
