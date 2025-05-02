fb1ed0eb090ae446f892ceccaf5305a5757b1f49
Aurelien Rodot <4189146+Rodot@users.noreply.github.com>
2025-05-02 18:09:52 +0200

    Handle non existant user/session/profile

--- app/lib/infrastructure/session_repository.dart
+++ app/lib/infrastructure/session_repository.dart
@@ -1,9 +1,25 @@
+import 'package:app/infrastructure/profile_repository.dart';
+import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<Session> getCurrentSession() async {
   final auth = Supabase.instance.client.auth;
   if (auth.currentSession == null) {
+    debugPrint("No current session, signing in anonymously.");
     await auth.signInAnonymously();
   }
+  try {
+    final userId = auth.currentSession!.user.id;
+    await fetch(userId);
+  } catch (e) {
+    debugPrint("Error fetching profile");
+    if (e.toString().contains("The result contains 0 rows")) {
+      debugPrint("Profile not found, re-signing in anonymously.");
+      await auth.signInAnonymously();
+    } else {
+      rethrow;
+    }
+  }
+  debugPrint("Signed in as ${auth.currentSession?.user.id}");
   return auth.currentSession!;
 }
